361bd4cf91314c1f93465ce8314ca426fcd2dfcd
cndaqiang <chendq@aliyun.com>
2020-07-25 18:58:03 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # packages
 在用的开源程序程序包
+
+html元素位置`html`<br>
+软件包位置`mirrors`<br>
+
+更新`index.html`
+```
+./build.sh
+```
+建立软连接
+```
+ ./ln.sh 
+ln -s /home/cndaqiang/code/packages/mirrors ~/mirrors
+```
---
+++ ln.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+##################################
+# Author : cndaqiang             #
+# Update : 2020-07-25            #
+# Build  : 2020-07-25            #
+# What   : 建立软连接            #
+#################################
+echo -e "ln -s $PWD/mirrors ~/mirrors"
+ln -s $PWD/mirrors ~/mirrors
