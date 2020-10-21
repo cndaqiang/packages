8b7e28fc06bd958c5b403c86977610d1b06d8361
cndaqiang <who@cndaqiang.ac.cn>
2020-10-21 23:20:45 +0800

    auto

---
+++ mirrors/XCrySDen/install.sh
@@ -0,0 +1,7 @@
+#Reference http://www.xcrysden.org/Download.html
+apt install tk libglu1-mesa libtogl2 libfftw3-3 libxmu6 \
+      imagemagick openbabel libgfortran5
+wget http://www.xcrysden.org/download/xcrysden-1.6.2-linux_x86_64-shared.tar.gz
+tar xcrysden-1.6.2-linux_x86_64-shared.tar.gz
+cd xcrysden-1.6.2-linux_x86_64-shared
+./xcrysden
